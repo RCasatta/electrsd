2a8a2977098e80bf04ad6784a08d2552b6a72aae
rajarshimaitra <rajarshi149@gmail.com>
2022-01-07 22:42:57 +0530

    Implement std::error::Error for crate::Error

--- src/lib.rs
+++ src/lib.rs
@@ -231,6 +231,14 @@ impl Drop for ElectrsD {
     }
 }
 
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for Error {}
+
 impl From<std::io::Error> for Error {
     fn from(e: std::io::Error) -> Self {
         Error::Io(e)
