2e89468d556bd3569d1a83b60a9ba25f8a3caf57
Riccardo Casatta <riccardo@casatta.it>
2023-03-13 09:19:04 +0100

    add feature for electrs 0.9.11

--- src/versions.rs
+++ src/versions.rs
@@ -13,9 +13,13 @@ const VERSION: &str = "esplora_a33e97e1a1fc63fa9c20a116bb92579bbf43b254";
 #[cfg(feature = "electrs_0_9_1")]
 const VERSION: &str = "v0.9.1";
 
+#[cfg(feature = "electrs_0_9_11")]
+const VERSION: &str = "v0.9.11";
+
 #[cfg(not(any(
     feature = "electrs_0_8_10",
     feature = "electrs_0_9_1",
+    feature = "electrs_0_9_11",
     feature = "esplora_a33e97e1",
 )))]
 const VERSION: &str = "NA";
@@ -23,6 +27,7 @@ const VERSION: &str = "NA";
 pub const HAS_FEATURE: bool = cfg!(any(
     feature = "electrs_0_8_10",
     feature = "electrs_0_9_1",
+    feature = "electrs_0_9_11",
     feature = "esplora_a33e97e1",
 ));
 
